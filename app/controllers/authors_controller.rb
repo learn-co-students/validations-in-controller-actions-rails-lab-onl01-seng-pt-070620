51a2b33b2aa6e3ed8063d3859b3e04f9eca433f1
Jbeltrez <beltrezjohn@gmail.com>
2020-12-03 15:59:02 -0500

    Done.

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -7,9 +7,22 @@ class AuthorsController < ApplicationController
   end
 
   def create
-    @author = Author.create!(author_params)
+    @author = Author.new (author_params)
+    if @author.valid? 
+      @author.save 
+      redirect_to author_path(@author)
+    else 
+      render :new 
+    end 
+    # # 
+    # # check if the author attempting to be created is valid with valid?, if it 
+    # # is go through the usual
+    # # else RE RENDER the form dont redirect so that we can show feedback 
+    # @author = Author.create!(author_params)
 
-    redirect_to author_path(@author)
+
+    # redirect_to author_path(@author)
+    # end 
   end
 
   private
